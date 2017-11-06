a6a24b94339d076dd72979a9b7bf71b7d640a12e
irhamputra <irhamputraprasetyo@gmail.com>
2017-11-06 05:07:13 +0100

    fix issue

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const API_KEY = 'bd0ea308ca71412eeb1c293f0b567d6b';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const API_KEY = '0dad158e9c086121cf76a1f96cba0b08';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-    const url = `${ROOT_URL}&q=${city},us`;
+    const url = `${ROOT_URL}&q=${city},de`;
     const req = axios.get(url);
 
     return{
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,9 @@
 import React, {Component} from "react";
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
     constructor(props){
         super(props);
 
@@ -14,12 +17,13 @@ export default class SearchBar extends Component {
     onFormSubmit(e){
         e.preventDefault();
 
-
+        this.props.fetchWeather(this.state.term);
+        this.setState({ term: '' })
     }
 
     render() {
         return (
-            <form className="input-group" onSubmit={this.onFormSubmit}>
+            <form className="input-group" onSubmit={this.onFormSubmit.bind(this)}>
                 <input
                     placeholder="Search City"
                     className="form-control"
@@ -34,3 +38,9 @@ export default class SearchBar extends Component {
         )
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
